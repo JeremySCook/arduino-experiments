cc1afd4cdbb6ce91bc80db814764307bedfc783a
Jeremy Cook <jscook55@gmail.com>
2024-05-23 16:36:51 -0400

    Update ADCTouch-ATtiny45.ino

--- attiny/ADCTouch-ATtiny45.ino
+++ attiny/ADCTouch-ATtiny45.ino
@@ -1,14 +1,25 @@
+//Have not gotten the serial portion to respond yet, but light
+//does blink based on contact
+
 #include <ADCTouch.h>
+#include <SoftwareSerial.h>
+
+#define RX 3
+#define TX 4
+SoftwareSerial Serial(RX, TX);
 
 void setup() 
 {
     pinMode(0, OUTPUT);
+    Serial.begin(9600);
+    Serial.println("Initializing...");
 } 
 
 void loop() 
 {
     int value0 = ADCTouch.read(2, __AVR_ATtiny45__);
-    if (value0 > 500) {
+    Serial.print("Analog Value: "); Serial.println(value0);
+    if (value0 > 600) {
     digitalWrite(0, HIGH);
     }
     else {
