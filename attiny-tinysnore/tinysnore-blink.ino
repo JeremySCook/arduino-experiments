36990c93084ed08ecd9f00d9262d0831ec8ae98d
Jeremy Cook <jscook55@gmail.com>
2024-05-28 14:41:29 -0400

    Update tinysnore-blink.ino

--- attiny-tinysnore/tinysnore-blink.ino
+++ attiny-tinysnore/tinysnore-blink.ino
@@ -1,15 +1,16 @@
 #include "tinysnore.h"
+int LEDPin = 3;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(0, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(0, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LEDPin, HIGH);  // turn the LED on (HIGH is the voltage level)
   snore(5000);                      // wait for a second
-  digitalWrite(0, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LEDPin, LOW);   // turn the LED off by making the voltage LOW
   snore(5000);                      // wait for a second
 }
