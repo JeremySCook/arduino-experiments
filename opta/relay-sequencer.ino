7ab2671a633c8504084c73e0c78c7d962f0f0145
Jeremy Cook <jscook55@gmail.com>
2024-06-19 21:16:18 -0400

    Update relay-sequencer.ino

--- opta/relay-sequencer.ino
+++ opta/relay-sequencer.ino
@@ -1,8 +1,13 @@
 //by @jeremyscook 6/19/2024
 //could go through different modes/BPM via onboard button???
 //hits onboard solenoids to act as a high hat, use SSRs to actuate solenoids
+//current version compiles, but not physically tested
 
 #include "OptaBlue.h"
+
+using namespace Opta; //not sure what this is for, but in examples
+
+DigitalStSolidExpansion stsolidExp;
 
 int timeOnDelay = 50; //how long device are active before retract (ms)
 int BPM = 120;
@@ -13,6 +18,8 @@ bool sequence1[] = {1,1,1,1,1,1,1,0}; //hat - hit tumbler with solenoid - USE RE
 bool sequence2[] = {0,0,1,0,0,0,1,0}; //snare - physical relay fires for snare sound (maybe multiple relays)
 
 void setup() {
+  OptaController.begin();
+  stsolidExp = OptaController.getExpansion(0); //expansion at index 0 is solid state relay
   pinMode(LED_RELAY1, OUTPUT);
   pinMode(LED_RELAY2, OUTPUT);
   pinMode(LED_RELAY3, OUTPUT);
@@ -26,9 +33,10 @@ void setup() {
 
 void loop() {
   for(int i = 0; i<8; i++){
-    if (sequence0[i] == 1){
+    if (sequence0[i] == 1){ //kick drum - solenoid
       //do something - maybe try lights first??
       digitalWrite(LED_RELAY1, HIGH);
+      stsolidExp.digitalWrite(0, HIGH);
     }
     if (sequence1[i] == 1){
       digitalWrite(LED_RELAY2, HIGH);
@@ -37,9 +45,9 @@ void loop() {
       digitalWrite(RELAY3, HIGH);
       digitalWrite(RELAY4, HIGH);
     }
-    if (sequence2[i] == 1){
+    if (sequence2[i] == 1){ //snare - solenoid - maybe hit tumbler?
       digitalWrite(LED_RELAY3, HIGH);
-
+      stsolidExp.digitalWrite(1, HIGH);
     }
     delay(timeOnDelay);
     //turn off outputs
@@ -52,6 +60,8 @@ void loop() {
     digitalWrite(RELAY2, LOW);
     digitalWrite(RELAY3, LOW);
     digitalWrite(RELAY4, LOW);
+    stsolidExp.digitalWrite(0, LOW);
+    stsolidExp.digitalWrite(1, LOW);
     delay(beatDelay - timeOnDelay); //time between beats hitting
     //restart the cycle for 8 beats
   }
