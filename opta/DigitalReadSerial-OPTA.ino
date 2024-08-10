6248ce8ae77e889414a6527c7de58afe8a5aa79c
Jeremy Cook <jscook55@gmail.com>
2024-08-10 09:51:40 -0400

    Create DigitalReadSerial-OPTA.ino

---
+++ opta/DigitalReadSerial-OPTA.ino
@@ -0,0 +1,31 @@
+/*
+  DigitalReadSerial
+
+  Reads a digital input on pin 2, prints the result to the Serial Monitor
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/DigitalReadSerial
+  modified for use with the opta - outputs on USB serial, as well as AUX serial
+  @jeremyscook Aug 10, 2024
+*/
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(BTN_USER, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int buttonState = digitalRead(BTN_USER);
+  // print out the state of the button:
+  Serial.println(buttonState);
+  Serial1.println(buttonState);
+  delay(100);  // delay in between reads for stability
+}
