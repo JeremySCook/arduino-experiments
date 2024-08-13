c9bc3e80f1e795419e61440ba85f5b292f1bc55d
Jeremy Cook <jscook55@gmail.com>
2024-08-12 21:43:50 -0400

    Update 1-inch-MIDI.ino

--- MIDI/1-inch-MIDI/1-inch-MIDI.ino
+++ MIDI/1-inch-MIDI/1-inch-MIDI.ino
@@ -1,6 +1,6 @@
 /*
 Works! Jul 10th, 2024 @JeremySCook
-
+New code w/ aux beat trigger compiles, but untested Aug 12th, 2024 @JeremySCook
 */
 
 #include <MIDI.h>
@@ -12,11 +12,15 @@ Works! Jul 10th, 2024 @JeremySCook
 #define LEDOut 1
 #define SIG1 1 // ADC Pin 1, corresponds to PB2
 #define SIG2 3
-#define Aux 4
+#define AUX 4
 
 bool noteInputStatus[] = {0,0,0,0};
 bool noteStatus[] = {0,0,0,0};
 int noteValue[] = {60, 61, 62, 63}; // keys corresponding to each note
+bool beatSelect = 0;
+unsigned long beatTime;
+int beatDelay = 500; //time between beats
+//int beatTime = 0;//also need a millis time that can be used between loops? beatTime
 
 // Define the SoftwareSerial port
 SoftwareSerial mySerial(MIDIIn, MIDIOut); // RX, TX
@@ -26,11 +30,24 @@ void setup() {
     // Initialize SoftwareSerial
     mySerial.begin(31250); // MIDI baud rate
     pinMode(LEDOut, OUTPUT);
+    pinMode(AUX, INPUT_PULLUP);
 }
 
 void loop() {
-    // Send a MIDI Note On message (Note, Velocity, Channel)
+    // Select if constant beat is on
+    if (beatSelect == 0){beat();}
+    bool AUXIn = digitalRead(AUX);
+    if (AUXIn == 1 && beatSelect == 0) {
+      beatSelect = 1;
+      //add in millis() time here?? don't think so
+      snore(500); //debounce 1/2 second
+    }
+    else if (AUXIn == 1 && beatSelect == 1) {
+      beatSelect = 0;
+      snore(500); //debounce 1/2 second
+    }
     
+    // Send a MIDI Note On message (Note, Velocity, Channel)
     int SIG1Value = analogRead(SIG1);
     noteInputStatus[0] = 0; noteInputStatus[1] = 0; //turns button status off by default
     if (SIG1Value > 800) {noteInputStatus[0] = 1; noteInputStatus[1] = 1;}
@@ -60,4 +77,16 @@ for (int i = 0; i < 4; i++) {
   }
   }
   snore(50); //polls every 50ms, sleeps otherwise
+}
+
+void beat(){
+  if (millis() - beatTime >= beatDelay){
+    midi2.sendNoteOn(60, 127, 1);
+    beatTime = millis();
+    digitalWrite(LEDOut, HIGH); 
+    snore(5);
+    digitalWrite(LEDOut, LOW);
+  }
+
+
 }
