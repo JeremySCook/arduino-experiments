d2a3edb5a14d01b488d10ea9f83c7f90e5e01053
Jeremy Cook <jscook55@gmail.com>
2024-08-20 16:41:54 -0400

    Update 1-inch-MIDI.ino

--- MIDI/1-inch-MIDI/1-inch-MIDI.ino
+++ MIDI/1-inch-MIDI/1-inch-MIDI.ino
@@ -1,6 +1,6 @@
 /*
 Works! Jul 10th, 2024 @JeremySCook
-New code w/ aux beat trigger compiles, but untested Aug 12th, 2024 @JeremySCook
+New code w/ potentiometer beat trigger compiles, but untested Aug 20th, 2024 @JeremySCook
 */
 
 #include <MIDI.h>
@@ -12,14 +12,16 @@ New code w/ aux beat trigger compiles, but untested Aug 12th, 2024 @JeremySCook
 #define LEDOut 1
 #define SIG1 1 // ADC Pin 1, corresponds to PB2
 #define SIG2 3
-#define AUX 4
+#define POTPIN 4
+#define NOTEDELAY 2
+#define POLLDELAY 40 //snore to save power before registering inputs
 
 bool noteInputStatus[] = {0,0,0,0};
 bool noteStatus[] = {0,0,0,0};
-int noteValue[] = {60, 61, 62, 63}; // keys corresponding to each note
-bool beatSelect = 0;
+int noteValue[] = {60, 62, 67, 69}; // C3, D3, G3 (notes for CAGE)
+//bool beatSelect = 0;
 unsigned long beatTime;
-int beatDelay = 500; //time between beats
+//int beatDelay = 500; //time between beats
 //int beatTime = 0;//also need a millis time that can be used between loops? beatTime
 
 // Define the SoftwareSerial port
@@ -30,23 +32,12 @@ void setup() {
     // Initialize SoftwareSerial
     mySerial.begin(31250); // MIDI baud rate
     pinMode(LEDOut, OUTPUT);
-    pinMode(AUX, INPUT_PULLUP);
+    pinMode(POTPIN, INPUT_PULLUP);
 }
 
 void loop() {
-    // Select if constant beat is on
-    if (beatSelect == 0){beat();}
-    bool AUXIn = digitalRead(AUX);
-    if (AUXIn == 1 && beatSelect == 0) {
-      beatSelect = 1;
-      //add in millis() time here?? don't think so
-      snore(500); //debounce 1/2 second
-    }
-    else if (AUXIn == 1 && beatSelect == 1) {
-      beatSelect = 0;
-      snore(500); //debounce 1/2 second
-    }
-    
+
+beat();
     // Send a MIDI Note On message (Note, Velocity, Channel)
     int SIG1Value = analogRead(SIG1);
     noteInputStatus[0] = 0; noteInputStatus[1] = 0; //turns button status off by default
@@ -65,28 +56,38 @@ for (int i = 0; i < 4; i++) {
     midi2.sendNoteOn(noteValue[i], 127, 1);
     noteStatus[i] = 1;
     digitalWrite(LEDOut, HIGH); 
-    snore(5);
+    snore(NOTEDELAY);
     digitalWrite(LEDOut, LOW);
   }
   else if (noteInputStatus[i] == 0 && noteStatus[i] == 1) {
     midi2.sendNoteOff(noteValue[i], 0, 1);
     noteStatus[i] = 0;
     digitalWrite(LEDOut, HIGH);
-    snore(5);
+    snore(NOTEDELAY);
     digitalWrite(LEDOut, LOW);
   }
   }
-  snore(50); //polls every 50ms, sleeps otherwise
+  snore(POLLDELAY); //polls every 50ms, sleeps otherwise
 }
 
+//scale bpm from 80 to 160 ==> a delay of 60/80 to 60/160 seconds ==> .75 seconds - .375 seconds
+//may be better to scale this directly: 50 off, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160 ==> 12 steps
+//use map function to scale between 5 and 12 in order to create set steps - do not execute at 5, multiply
+//by 10 otherwise
+
 void beat(){
-  if (millis() - beatTime >= beatDelay){
-    midi2.sendNoteOn(60, 127, 1);
+  int PotMap = map(analogRead(POTPIN), 0, 1023, 5, 12);
+  int BPMMap = PotMap*10; // scales mapped value up to a BPM value
+  int beatDelay = (60*1000/BPMMap); // gives a value in milliseconds between each beat
+  
+  if (BPMMap > 50){ //take out low values to turn off
+    if (millis() - beatTime >= beatDelay){
+    midi2.sendNoteOn(40, 127, 1); // electric snare per general MIDI drum map, could use different instrument (last value)?
     beatTime = millis();
     digitalWrite(LEDOut, HIGH); 
-    snore(5);
+    snore(NOTEDELAY);
     digitalWrite(LEDOut, LOW);
   }
-
+  }
 
 }
