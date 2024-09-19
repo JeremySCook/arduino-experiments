e6e1602784fa1ea9d5ff07036266b83a4a92b229
Jeremy Cook <jscook55@gmail.com>
2024-09-18 21:58:06 -0400

    Update OptaAutoDrums.ino

--- MIDI/AutoDrums/OptaAutoDrums.ino
+++ MIDI/AutoDrums/OptaAutoDrums.ino
@@ -1,6 +1,22 @@
+
 //For Arduino Opta input w/ SSRs
-//Compiled but untested as of Sept 16, 2024
+//Compiled but untested as of Sept 18, 2024
 //Add code to show MIDI signals on OLED via new custom adapter?
+
+//setup for SSD1306 module - need splash screen etc?
+
+#include <SPI.h> //needed??
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//End setup for SSD1306 module
 
 #include <MIDI.h>  // Add Midi Library
 #include "OptaBlue.h"
@@ -12,7 +28,17 @@ DigitalStSolidExpansion stsolidExp;
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, midiOpta);
 
 void setup() {
+  OptaController.begin(); //maybe needed for SSR expansion? Not sure.
+  stsolidExp = OptaController.getExpansion(0); //expansion at index 0 is solid state relay needed???
   pinMode (LED_BUILTIN, OUTPUT); // Set Arduino board pin 13 to output
+  
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' fon
+  display.setRotation(1); //rotates the screen 90 degrees
+  
   midiOpta.begin(MIDI_CHANNEL_OMNI); // Initialize the Midi Library.
   // OMNI sets it to listen to all channels.. MIDI.begin(2) would set it 
   // to respond to notes on channel 2 only.
@@ -32,12 +58,23 @@ void loop() { // Main loop
 // It will be passed bytes for Channel, Pitch, and Velocity
 void MyHandleNoteOn(byte channel, byte pitch, byte velocity) { 
   digitalWrite(LED_BUILTIN, HIGH);  //Turn LED on
+  Serial.print("Pitch On "); Serial.println(pitch);
+  display.clearDisplay();
+  display.write("Pitch on: "); display.write(pitch);
   if (pitch == 60) { // hex 3C, note 3C
       stsolidExp.digitalWrite(0, HIGH);
       stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
   }
   else if (pitch == 61) { // hex 3E, note 3D
       stsolidExp.digitalWrite(1, HIGH);
+      stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
+  }
+  else if (pitch == 62) { // hex 3E, note 3D
+      stsolidExp.digitalWrite(2, HIGH);
+      stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
+  }
+  else if (pitch == 63) { // hex 3E, note 3D
+      stsolidExp.digitalWrite(3, HIGH);
       stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
   }
 }
@@ -48,6 +85,9 @@ void MyHandleNoteOn(byte channel, byte pitch, byte velocity) {
 // It will be passed bytes for Channel, Pitch, and Velocity
 void MyHandleNoteOff(byte channel, byte pitch, byte velocity) { 
   digitalWrite(LED_BUILTIN,LOW);  //Turn LED off
+  Serial.print("Pitch Off "); Serial.println(pitch);
+  display.clearDisplay();
+  display.write("Pitch off: "); display.write(pitch);
     if (pitch == 60) { // hex 3C, note 3C
       stsolidExp.digitalWrite(0, LOW);
       stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
@@ -55,5 +95,13 @@ void MyHandleNoteOff(byte channel, byte pitch, byte velocity) {
     else if (pitch == 61) { // hex 3E, note 3D
       stsolidExp.digitalWrite(1, LOW);
       stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
+    }
+    else if (pitch == 62) { // hex 3E, note 3D
+      stsolidExp.digitalWrite(2, LOW);
+      stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
   }
+    else if (pitch == 63) { // hex 3E, note 3D
+      stsolidExp.digitalWrite(3, LOW);
+      stsolidExp.updateDigitalOutputs(); //UPDATE SOLID STATE OUTPUTS
+    }
 }
