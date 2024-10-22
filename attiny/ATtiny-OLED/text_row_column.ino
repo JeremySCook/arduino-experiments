8ab50804d14c58f41aa9f12f9880f538fc99b1f0
Jeremy Cook <jscook55@gmail.com>
2024-10-22 14:35:29 -0400

    Create text_row_column.ino

---
+++ attiny/ATtiny-OLED/text_row_column.ino
@@ -0,0 +1,33 @@
+#include <TinyWireM.h>
+#include <Tiny4kOLED.h>
+int delayValue = 500;
+
+void setup() {
+  oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
+  // Two fonts are supplied with this library, FONT8X16 and FONT6X8
+  oled.setFont(FONT8X16);
+  oled.on();
+  oled.clear();
+}
+
+void loop() {
+  for (int i = 0; i < 7; i = i+2){
+    //oled.clear();
+    oled.setCursor(0, i);
+    oled.print("Row # "); oled.print(i);
+    delay(delayValue);
+  }
+delay(1000);
+oled.clear();
+
+  for (int i = 0; i < 7; i = i+2){
+    //oled.clear();
+    int j = i * 16;
+    oled.setCursor(j, i);
+    oled.print("Col # "); oled.print(j); //last value will wrap
+    delay(delayValue);
+  }
+delay(1000);
+oled.clear();
+
+}
