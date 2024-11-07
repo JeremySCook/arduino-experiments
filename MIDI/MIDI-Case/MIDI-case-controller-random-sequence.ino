479d15867577281381745b0ccff9a269debd6ede
Jeremy Cook <jscook55@gmail.com>
2024-11-07 16:19:37 -0500

    Update MIDI-case-controller-random-sequence.ino

--- MIDI/MIDI-Case/MIDI-case-controller-random-sequence.ino
+++ MIDI/MIDI-Case/MIDI-case-controller-random-sequence.ino
@@ -4,14 +4,14 @@ void buttonSequence(){
       buttonState = 1;
       //MIDI.sendNoteOn(48, 1, 1); //for testing
       beatOn = 1;
-      delay(50);
+      delay(SEQUENCEDELAY);
     }
   }
   if(buttonState == 1){
     if(digitalRead(auxRightButton) == 1){ //button released, leave beat on
       buttonState = 2;
       //MIDI.sendNoteOn(48, 2, 1); //for testing
-      delay(50);
+      delay(SEQUENCEDELAY);
     }    
   }
   if(buttonState == 2){
@@ -21,26 +21,26 @@ void buttonSequence(){
       beatOn = 0; // controls autobeat
       beatStep = 0; //reset auto beat seqeunce to step 0 when turned off
       randomNotesOn = 1; //turn randomNotes routine on
-      delay(50);
+      delay(SEQUENCEDELAY);
     }
   }
   if(buttonState == 3){
     if(digitalRead(auxRightButton) == 1) //button released, leave random notes on
       buttonState = 4;
       //MIDI.sendNoteOn(48, 4, 1); //for testing
-      delay(50);
+      delay(SEQUENCEDELAY);
   }
   if(buttonState == 4){
     if(digitalRead(auxRightButton) == 0) //button depressed
       buttonState = 5;
       //MIDI.sendNoteOn(48, 5, 1); //for testing
       randomNotesOn = 1; //turn randomNotes routine off
-      delay(50);
+      delay(SEQUENCEDELAY);
   }
   if(buttonState == 5){
     if(digitalRead(auxRightButton) == 1) //button released, go to start conditions
       buttonState = 0;
       randomNotesOn = 0; //turn randomNotes routine off
-      delay(50);
+      delay(SEQUENCEDELAY);
   }
 }
